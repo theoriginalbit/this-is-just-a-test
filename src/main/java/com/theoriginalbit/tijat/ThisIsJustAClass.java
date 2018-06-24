46ca53be07c9dd1aaad0ed838937dd367eff1245
theoriginalbit <theoriginalbit@gmail.com>
2018-06-24 15:17:41 +1000

    Add javadoc

--- src/main/java/com/theoriginalbit/tijat/ThisIsJustAClass.java
+++ src/main/java/com/theoriginalbit/tijat/ThisIsJustAClass.java
@@ -1,21 +1,45 @@
 package com.theoriginalbit.tijat;
 
+/**
+ * A simple testable class.
+ */
 @SuppressWarnings("WeakerAccess")
 public class ThisIsJustAClass {
     private final String custom;
 
+    /**
+     * Creates an instance of this class with a custom string for {@link #getHelloCustom()}.
+     *
+     * @param custom a custom string
+     */
     public ThisIsJustAClass(String custom) {
         this.custom = custom;
     }
 
+    /**
+     * Get a greeting string that greets the world.
+     *
+     * @return the greeting
+     */
     public String getHelloWorld() {
         return "Hello, World";
     }
 
+    /**
+     * Get a greeting string that greets the supplied name.
+     *
+     * @param name the name to greet
+     * @return the greeting
+     */
     public String getHello(String name) {
         return "Hello, " + name;
     }
 
+    /**
+     * Get a greeting string that greets the custom string provided in {@link #ThisIsJustAClass(String)}.
+     *
+     * @return the greeting
+     */
     public String getHelloCustom() {
         return "Hello, " + custom;
     }
